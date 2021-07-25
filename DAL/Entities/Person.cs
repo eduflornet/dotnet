37e3fd6c8b7630130111be4c633ee8adaadb4e34
Eduardo Flores <@eduflornet>
2021-07-25 12:09:06 +0200

    Added DAL implementation with Address and Person Entities.

---
+++ DAL/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    [Table("Persons", Schema = "dbo")]
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # NET5
-- Refreshing LINQ 
 - Applying C# 9
+- Refreshing LINQ 
+- Applying Entity Framewrk Core 5
 - Using VSCode
 ## References:
 - [What's new in .NET 5](https://docs.microsoft.com/en-us/dotnet/core/dotnet-five)
 - [What's new in C# 9.0](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9)
 - [Language Integrated Query (LINQ)](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/)
+- [Entity Framework Core 5](https://docs.microsoft.com/en-us/ef/core/)
